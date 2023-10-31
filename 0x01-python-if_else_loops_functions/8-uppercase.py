5a1a772c3690bfd2ce03f50427dc0974e586c71f
rashaaz <rashaabdelaziz111@gmail.com>
2023-10-30 23:29:56 -0700

    add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
+
+
+def uppercase(str):
+    for char in str:
+        print("{:c}"
+                .format(ord(char) if not islower(char) else ord(char) - 32), 
+                end="")
+    print("")
