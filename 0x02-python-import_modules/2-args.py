42a9318d1d0c896f63ac58dfbc992b70e29610f4
rashaaz <rashaabdelaziz111@gmail.com>
2023-11-01 21:40:31 -0700

    modfy 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    num_ar = len(sys.argv) - 1
+    if num_ar == 0:
+        print("0 arguments.")
+    elif num_ar == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_ar))
+        for i in range(num_ar):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
