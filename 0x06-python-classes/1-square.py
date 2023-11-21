cf279ac105ac8024f97f3072633d0ba0304e0049
rashaaz <rashaabdelaziz111@gmail.com>
2023-11-20 20:19:57 -0800

    add  1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""the Square class."""
+
+
+class Square:
+    """The defines for the Square class"""
+
+    def __init__(self, size):
+        """This is the constructor.
+
+        Args:
+            size: The square size.
+        """
+        self.__size = size
