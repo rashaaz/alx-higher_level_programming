9884961a5713473a70c9da0aad6c5dd60fcdda05
rashaaz <rashaabdelaziz111@gmail.com>
2023-11-23 21:55:41 -0800

    add 102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <unicodeobject.h>
+#include <Python.h>
+#include <stdlib.h>
+
+
+/**
+ * print_python_string - Prints info about Python strings
+ * @p: PyObject representing a string
+ */
+void print_python_string(PyObject *p)
+{
+	wprintf(L"[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		wprintf(L"  [ERROR] Invalid String Object\n");
+		return;
+	}
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		wprintf(L"  type: compact ascii\n");
+	else
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
+}
+
