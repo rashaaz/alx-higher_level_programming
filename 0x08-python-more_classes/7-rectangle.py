c0663222bff5051bd45106fe2cb0e61cab4ec12d
rashaaz <rashaabdelaziz111@gmail.com>
2023-11-26 22:14:53 -0800

    add 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -65,7 +65,7 @@ class Rectangle:
     def __repr__(self):
         """Return a string representation of the rectangle for reproduction"""
         return "Rectangle(" + str(self.__width)
-    + ", " + str(self.__height) + ")"
+        + ", " + str(self.__height) + ")"
 
     def __del__(self):
         """Print a message when an instance of Rectangle is deleted"""
