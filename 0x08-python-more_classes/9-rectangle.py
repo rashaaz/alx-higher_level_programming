21e8a86f3e8f07fda55a1e03c4e41773e62ff180
rashaaz <rashaabdelaziz111@gmail.com>
2023-11-27 02:00:58 -0800

    modfy

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize the rectangle with optional width and height"""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
