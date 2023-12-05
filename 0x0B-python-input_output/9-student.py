0b3e7cd6e133d8a39ef23d534211d67de770cee1
rashaaz <rashaabdelaziz111@gmail.com>
2023-12-04 21:26:30 -0800

    add 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class Student that defines a student with public instance"""
+
+
+class Student:
+    """Class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the public attributes of the student.
+        """
+        return self.__dict__
