ee4c7df16636d04e51e8a78c066b567e730de78c
rashaaz <rashaabdelaziz111@gmail.com>
2024-01-16 20:57:51 -0800

    add 8-cities_of_california_subquery

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- 8-cities_of_california_subquery.sql
+-- Selecting cities of California using a subquery
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
