2bb0602e79c1cda08b950bbf3e9691a5fd1b3ceb
rashaaz <rashaabdelaziz111@gmail.com>
2024-01-16 19:44:45 -0800

    add 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Creating user user_0d_1 if not exists and giv it all privileges
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
