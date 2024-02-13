58ddfcaccb9c5fe95a7b8497e1b8551cf2b4e3c9
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-12 21:09:43 -0800

    add 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -16,6 +16,17 @@ class Rectangle {
       console.log(m);
     }
   }
+
+  rotate () {
+    const ss = this.width;
+    this.width = this.height;
+    this.height = ss;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
