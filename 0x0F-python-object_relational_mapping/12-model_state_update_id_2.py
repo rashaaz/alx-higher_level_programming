f77fbe065e3eaf9bf0dcfe9d5b569883adae9814
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-14 23:28:31 -0800

    add 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script to connect to a MySQL database using SQLAlchemy,
+update the name of a state with ID 2, and commit the changes
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    sscctt = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=sscctt)
+    session = Session()
+
+    sr = session.query(State).filter_by(id=2).first()
+    sr.name = "New Mexico"
+    session.commit()
