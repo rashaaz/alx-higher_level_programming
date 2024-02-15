1c7cfe936c65fc1043c15b58f452bd2f09f62a3e
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-14 23:50:43 -0800

    add 14-model_city_fetch_by_state.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines the City class for representing
+cities in a database
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """A class to represent cities in the database"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
