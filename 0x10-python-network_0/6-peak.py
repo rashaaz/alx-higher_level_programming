768db850e38477997572880376621266ac84f7fc
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-29 02:03:47 -0800

    modfy

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
+"""task 6 Python - Network"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
+    """Find a peak in a list of unsorted integers"""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]
